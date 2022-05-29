a7de52dcb6985818f5438803389dd3d6c2cb4393
Zuriel_Nicholas <2001555@sit.singaporetech.edu.sg>
2022-05-29 14:53:47 +0800

    number.py

---
+++ In_py/numbers.py
@@ -0,0 +1,18 @@
+a = 12
+b = 3
+c = 15.0
+d = 4.0
+
+print("a + b =",a + b, "Addition")
+print("a - b =",a - b, "subtraction")
+print("a * b =",a * b, "multiplication")
+print("a / b =",a / b, "division")
+print("a // b =",a // b, "Floor division")
+print("a % b =",a % b, "modulus")
+print("\n\n")
+print("c + d =", c + d, "addition")
+print("c - d =", d - c, "subtraction")
+print("c * d =", c * d, "multiplication")
+print("c / d =", c / d, "division")
+print("c // d =", c // d, "floor division")
+print("c % d =", c % d, "modulus" )
